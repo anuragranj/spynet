950641c11c678a7c415271d7e1e41d918e62ae47
Anurag Ranjan <anurag.ranjan@tue.mpg.de>
2016-09-23 17:39:51 +0200

    added stn

---
+++ extras/stnbhwd/build/CMakeFiles/custn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/custn.dir/custn_generated_init.cu.o"
+  "libcustn.pdb"
+  "libcustn.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/custn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
